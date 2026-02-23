c2a641bc650e5b9c7e0647be38c8384597af5007
Adrian <alijevicadrian@seznam.cz>
2026-02-23 23:25:27 +0100

    Druhý Commit

--- main.py
+++ main.py
@@ -9,10 +9,13 @@ def overeni(cesta):
     return os.path.exists(cesta)
 
 def hledani_v_podslozkach(start_cesta, hledany_nazev):
+
+    seznam = []
     for aktualni_cesta, slozky, soubory in os.walk(start_cesta):
         if hledany_nazev in slozky or hledany_nazev in soubory:
-            return os.path.join(aktualni_cesta, hledany_nazev)
-    return None
+            cesta = os.path.join(aktualni_cesta, hledany_nazev)
+            seznam.append(cesta)
+    return seznam
 
 # řídicí kod pro funkce
 podle_nazvu = hledani()
@@ -21,7 +24,13 @@ nalezeno = hledani_v_podslozkach(start_cesta, podle_nazvu)
 
 # výpis bud s uspěsným nalezem nebo negatvním
 if nalezeno:
-    os.startfile(nalezeno)
+    for index, hodnota in enumerate(nalezeno,1):
+        print(index,hodnota)
+
+    volba = input("vyber číslo:")
+    index = int(volba) - 1
+
+    os.startfile(nalezeno[index])
     print("Soubor nebo složka byla nalezena")
 else:
     print("Soubor ani složka nebyly nalezeny")
